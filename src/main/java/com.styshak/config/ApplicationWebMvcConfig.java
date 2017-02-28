c1b1336647d0972a5aa70694667d63c40f9b692f
Sergey <SorcerOK@bigmir.net>
2017-02-28 07:35:08 +0200

    Initial commit

---
+++ src/main/java/com.styshak/config/ApplicationWebMvcConfig.java
@@ -0,0 +1,66 @@
+package com.styshak.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+public class ApplicationWebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+
+    /* Localization section */
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource msg = new ReloadableResourceBundleMessageSource();
+        msg.setBasename("/WEB-INF/locales/messages");
+        msg.setDefaultEncoding("UTF-8");
+        return msg;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
+
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+
+<html>
+<head>
+    <title><spring:message code="page_title"/></title>
+</head>
+<body>
+    <h1>Hello, ${name}</h1>
+</body>
+</html>
